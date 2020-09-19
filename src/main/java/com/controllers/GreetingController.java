16c079cfb2790ce97273319615ccc6d239684b98
pedrohubner <pedronhubner@gmail.com>
2020-09-18 21:27:06 -0300

    Deletando classes

--- src/main/java/com/controllers/GreetingController.java
+++
@@ -1,9 +0,0 @@
-package com.controllers;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-
-public interface GreetingController {
-
-//    @RequestMapping(value = "/greeting")
-//    String greeting;
-}
